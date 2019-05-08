0aba70e19944487ab24281c54be7481d96aa39bb
Jarvis Yang <jarvisgyang@gmail.com>
2019-05-08 08:41:26 -0500

    implement jest testing; added server.test.js file

--- server/server.js
+++ server/server.js
@@ -37,3 +37,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
+
+// Export our app so that we can use it in tests
+module.exports = app;
